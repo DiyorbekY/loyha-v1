ddcbe3d6060124723b5b919e5b12b48fd63ee667
Diyorbek <diyorbekyoldoshev846@gmail.com>
2025-07-15 11:50:21 +0500

    tests

--- config/settings.py
+++ config/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'loyha_db'),
-        'USER': os.getenv('DB_USER', 'loyha_user'),
+        'USER': os.getenv('DB_USER', 'loyha_admin'),
         'PASSWORD': os.getenv('DB_PASSWORD', 'root'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
